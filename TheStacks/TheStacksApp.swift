d3c1f53a4a670c971fcb55f6a0a529e23ba8fce5
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2024-12-22 13:32:18 -0800

    Initial Commit

---
+++ TheStacks/TheStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStacksApp.swift
+//  TheStacks
+//
+//  Created by Daryoush Ghoreishi on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TheStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheStacksTests/TheStacksTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheStacksTests.swift
+//  TheStacksTests
+//
+//  Created by Daryoush Ghoreishi on 12/22/24.
+//
+
+import Testing
+@testable import TheStacks
+
+struct TheStacksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
