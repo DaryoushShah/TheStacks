aa34a86b3f1bbef8f93a453d4b5f474645f2b9f6
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2024-12-22 22:17:28 -0800

    Implemented containers and text

--- TheStacks/ContentView.swift
+++ TheStacks/ContentView.swift
@@ -10,12 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("VStack Item 1")
+            Text("VStack Item 2")
+            Divider()
+                .background(Color.black)
+            Spacer()
+            Text("VStack Item 3")
+            
+            HStack {
+                Spacer()
+                Text("HStack Item 1")
+                Spacer()
+                Divider()
+                    .background(Color.black)
+                Spacer()
+                Text("HStack Item 2")
+                Spacer()
+                Divider()
+                    .background(Color.black)
+                Spacer()
+                Text("HStack Item 3")
+                Spacer()
+            }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .center
+            )
+            .background(Color.red)
+            
+            ZStack {
+                Text("ZStack Item 1")
+                    .padding()
+                    .background(Color.green)
+                    .opacity(0.8)
+                
+                Text("ZStack Item 2")
+                    .padding()
+                    .background(Color.green)
+                    .offset(x: 80, y: -400)
+            }
         }
-        .padding()
+        .background(Color.blue)
     }
 }
 
